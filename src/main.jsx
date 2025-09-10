59a1947a19e40e65ef0378ab758276d7196555c3
kenrosh2407-alt <kenrosh2407@gmail.com>
2025-09-10 16:37:48 +0000

    fix: ensure root div + app mount

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { registerSW } from 'virtual:pwa-register'
-registerSW({ immediate: true })
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('app')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
